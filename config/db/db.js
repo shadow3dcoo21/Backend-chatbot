eed393c3735c0f6019a4459b01ac04e9883978eb
Mijael Cama <mijael.cama@tecsup.edu.pe>
2025-07-11 12:39:29 -0500

    revert changes of db.js to the original state

--- config/db/db.js
+++ config/db/db.js
@@ -1,24 +1,16 @@
-// cargar variables de entorno
-import 'dotenv/config';
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DB_SERVER_MONGO, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      dbName: process.env.DB_NAME_MONGO,
     });
-    console.log(
-      `MongoDB conectado a "${process.env.DB_NAME_MONGO}"`
-    );
+    console.log('MongoDB conectado');
   } catch (err) {
-    console.error(
-      'Error de conexión a MongoDB:',
-      err.message
-    );
-    process.exit(1);
+    console.error('Error de conexión a MongoDB:', err.message);
+    process.exit(1);  // Salir si hay error
   }
 };
 
-export default connectDB;
+module.exports = connectDB;
